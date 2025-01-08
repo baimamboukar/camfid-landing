d0ceffe3a3d5759e863eb6256ec11623b1c49e02
baimamboukar <baimamboukar@gmail.com>
2025-01-08 16:32:59 +0200

    chore(auth): add watch effect in confirm page

--- app/pages/confirm.vue
+++ app/pages/confirm.vue
@@ -5,6 +5,17 @@ definePageMeta({
 });
 
 const email = useState("signupEmail");
+const user = useSupabaseUser();
+
+watch(
+  user,
+  () => {
+    if (user.value) {
+      return navigateTo("/");
+    }
+  },
+  { immediate: true }
+);
 </script>
 
 <template>
